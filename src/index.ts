ae894934f31fd3064b05f28856040093bb35912e
Matthew Beech <matthew_beech@live.co.uk>
2021-01-27 01:39:03 +0000

    Moved event handlers and login to constructor

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,7 @@
-import { Message } from 'discord.js';
 import dotenv from 'dotenv';
-import { prefix } from './config.json';
-import { logger } from './logger';
 import { Cadence } from './models/client';
-import { MusicSettings } from './models/musicSettings';
-import { checkUserCanRun, parseMessage } from './utils/utils';
 
 // Initialise dotenv config - if you're doing config that way
 dotenv.config();
 
-export const client = new Cadence();
-
-client.on('ready', () => {
-    // This event will run if the bot starts, and logs in, successfully.
-    logger.info(
-        `Cadence is playing to ${client.users.cache.size} users in ${client.guilds.cache.size} guilds.`
-    );
-
-    // Example of changing the bot's playing game to something useful. `client.user` is what the
-    // docs refer to as the "ClientUser".
-    client.user?.setActivity(`sweet beats`);
-
-    // On start up create music settings for each guild
-    client.guilds.cache.forEach((guild) =>
-        client.musicSettings.set(guild.id, new MusicSettings())
-    );
-});
-
-client.on('guildCreate', async (guild) => {
-    // This event triggers when the bot joins a guild.
-    logger.info(
-        `New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`
-    );
-
-    // Create settings for the new guild
-    client.musicSettings.set(guild.id, new MusicSettings());
-});
-
-client.on('guildDelete', (guild) => {
-    // this event triggers when the bot is removed from a guild.
-    logger.info(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-});
-
-// This event will run on every single message received, from any channel or DM.
-client.on('message', async (message: Message) => {
-    // Ignore bots, messages from outside guilds and messages without the prefix
-    if (
-        !message.content.startsWith(prefix) ||
-        message.author.bot ||
-        !message.guild
-    ) {
-        return;
-    }
-
-    logger.debug('Entered on message.');
-
-    const { command, args } = parseMessage(message);
-
-    const runnableCommand = client.commands.get(command);
-
-    if (runnableCommand) {
-        if (
-            checkUserCanRun(
-                message.member!,
-                runnableCommand.permissions,
-                runnableCommand.roles
-            )
-        ) {
-            runnableCommand.run(message, args);
-        }
-    }
-});
-
-client.login(process.env.TOKEN);
+export const client = new Cadence(process.env.TOKEN);
--- src/models/client.ts
+++ src/models/client.ts
@@ -1,7 +1,11 @@
-import { Client, ClientOptions, Collection } from 'discord.js';
+import { Client, ClientOptions, Collection, Message } from 'discord.js';
 import { Command } from './command';
 import fs from 'fs';
 import { MusicSettings } from './musicSettings';
+import { client } from '..';
+import { logger } from '../logger';
+import { parseMessage, checkUserCanRun } from '../utils/utils';
+import { prefix } from '../config.json';
 
 /**
  * An extension of the discord.js Client class, which also includes commands.
@@ -11,7 +15,7 @@ export class Cadence extends Client {
 
     musicSettings: Collection<string, MusicSettings> = new Collection();
 
-    constructor(options?: ClientOptions) {
+    constructor(token?: string, options?: ClientOptions) {
         super(options);
 
         // Initialises all the commands found in the /commands directory
@@ -25,7 +29,70 @@ export class Cadence extends Client {
                 }
             });
         }
+
+        this.on('ready', () => {
+            // This event will run if the bot starts, and logs in, successfully.
+            logger.info(
+                `Cadence is playing to ${client.users.cache.size} users in ${client.guilds.cache.size} guilds.`
+            );
+
+            // Example of changing the bot's playing game to something useful. `client.user` is what the
+            // docs refer to as the "ClientUser".
+            client.user?.setActivity(`sweet beats`);
+
+            // On start up create music settings for each guild
+            this.guilds.cache.forEach((guild) =>
+                this.musicSettings.set(guild.id, new MusicSettings())
+            );
+        });
+
+        this.on('guildCreate', async (guild) => {
+            // This event triggers when the bot joins a guild.
+            logger.info(
+                `New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`
+            );
+
+            // Create settings for the new guild
+            this.musicSettings.set(guild.id, new MusicSettings());
+        });
+
+        this.on('guildDelete', (guild) => {
+            // this event triggers when the bot is removed from a guild.
+            logger.info(
+                `I have been removed from: ${guild.name} (id: ${guild.id})`
+            );
+        });
+
+        // This event will run on every single message received, from any channel or DM.
+        this.on('message', async (message: Message) => {
+            // Ignore bots, messages from outside guilds and messages without the prefix
+            if (
+                !message.content.startsWith(prefix) ||
+                message.author.bot ||
+                !message.guild
+            ) {
+                return;
+            }
+
+            logger.debug('Entered on message.');
+
+            const { command, args } = parseMessage(message);
+
+            const runnableCommand = client.commands.get(command);
+
+            if (runnableCommand) {
+                if (
+                    checkUserCanRun(
+                        message.member!,
+                        runnableCommand.permissions,
+                        runnableCommand.roles
+                    )
+                ) {
+                    runnableCommand.run(message, args);
+                }
+            }
+        });
+
+        this.login(token);
     }
 }
-
-async function Blah(client: Cadence) {}
