3306d55d66c19ab5289312df7f63bc4616f38d85
Matt Beech <matthew_beech@live.co.uk>
2021-02-10 17:36:33 +0000

    Removed the reload command as it was more hassle than it was worth

---
+++ src/commands/invite.ts
@@ -0,0 +1,20 @@
+import { Message, MessageEmbed } from 'discord.js';
+import { DeveloperCommand } from '../models/developerCommand';
+
+export default class extends DeveloperCommand {
+    constructor() {
+        super({
+            name: 'invite',
+            description:
+                'Get my invite link to invite Cadence to other servers!',
+        });
+    }
+
+    run(message: Message) {
+        const messageToReturn = new MessageEmbed({ title: 'Invite Link' });
+        const inviteLink =
+            'https://discord.com/api/oauth2/authorize?scope=bot&client_id=';
+        messageToReturn.setFooter(`${inviteLink}${this.cadence.user?.id}`);
+        message.channel.send(messageToReturn);
+    }
+}
